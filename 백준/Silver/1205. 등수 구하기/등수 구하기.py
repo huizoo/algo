135152ceb0e1d47836cbf6b79203d7e4d0975c51
huizoo <ajtwlsgmlwn@naver.com>
2025-12-20 00:04:09 +0900

    [Silver IV] Title: 등수 구하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -2,15 +2,19 @@ import sys
 input = sys.stdin.readline
 
 N, M, P = map(int, input().split())
-
 if N > 0:
     rank = list(map(int, input().split()))
-    rank.sort()
+    rank.sort(reverse=True)
 
-    if N == P and M <= rank[0]:
+    if N == P and M <= rank[-1]:
         print(-1)
     else:
-        x = sum(1 for x in rank if x > M)
-        print(x + 1)
+        higher = 0
+        for x in rank:
+            if x > M:
+                higher += 1
+            else:
+                break
+        print(higher + 1)
 else:
     print(1)
