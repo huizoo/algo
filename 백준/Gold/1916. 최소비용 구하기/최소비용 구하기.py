60fd1d122626f9d32f034bdc8a3f3c2da11ab9b3
huizoo <ajtwlsgmlwn@naver.com>
2025-12-22 14:43:14 +0900

    [Gold V] Title: 최소비용 구하기, Time: 312 ms, Memory: 58232 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,30 @@
+import sys, heapq
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+arr = [[] for _ in range(1001)]
+for _ in range(M):
+    start, end, cost = map(int, input().split())
+    arr[start].append((end, cost))
+
+start, end = map(int, input().split())
+
+dist = [1e11]*(1001)
+dist[start] = 0
+
+heap = [(0, start)]
+
+while heap:
+    cost, now = heapq.heappop(heap)
+    if dist[now] < cost:
+        continue
+    
+    for nxt, weight in arr[now]:
+        new_cost = cost + weight
+        if dist[nxt] > new_cost:
+            dist[nxt] = new_cost
+            heapq.heappush(heap, (new_cost, nxt))
+
+print(dist[end])
