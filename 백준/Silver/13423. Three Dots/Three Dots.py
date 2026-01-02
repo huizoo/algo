5dd39407ae3ebdb57d6c043922b815d447c88408
huizoo <ajtwlsgmlwn@naver.com>
2026-01-02 17:01:20 +0900

    [Silver II] Title: Three Dots, Time: 356 ms, Memory: 110968 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.py
+++ 백준/Silver/13423. Three Dots/Three Dots.py
@@ -4,11 +4,21 @@ input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     N = int(input())
-    arr = sorted(list(map(int, input().split())))
-    Set = set(arr)
+    arr = sorted(map(int, input().split()))
     cnt = 0
-    for i in range(N):
-        for j in range(i+1, N):
-            if 2*arr[j]-arr[i] in Set:
+    for j in range(1, N-1):
+        i = 0
+        k = N-1
+        while i < j and j < k:
+            l = 2*arr[j]
+            Sum = arr[i]+arr[k]
+            if Sum == l:
                 cnt += 1
+                i += 1
+                k -= 1
+            elif Sum > l:
+                k -= 1
+            else:
+                i += 1
+            
     print(cnt)
