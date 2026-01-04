9c1b966287ad82bcdff55bca0a66bb26c05fc04d
huizoo <ajtwlsgmlwn@naver.com>
2026-01-05 00:54:48 +0900

    [Gold IV] Title: 나는 정말 휘파람을 못 불어, Time: 1428 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+M = 1000000007
+
+N = int(input())
+S = input().rstrip()
+
+w = wh = whe = whee = 0
+
+for x in S:
+    if x == 'W':
+        w += 1
+    elif x == 'H':
+        wh += w
+    elif x == 'E':
+        whee = whee*2 + whe
+        whe += wh
+
+print(whee % M)
