dd61620ff1907c384d3b94cae1573bbe5039b6d5
huizoo <ajtwlsgmlwn@naver.com>
2026-01-06 13:37:07 +0900

    [Silver II] Title: 내 뒤에 나와 다른 수, Time: 524 ms, Memory: 260440 KB -BaekjoonHub

---
+++ 백준/Silver/24523. 내 뒤에 나와 다른 수/내 뒤에 나와 다른 수.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+answer = [0]*N
+answer[N-1] = -1
+Min = N-1
+Min2 = N
+for i in range(N-2, -1, -1):
+    a, b = arr[i], arr[Min]
+    if a == b:
+        if Min2 == N:
+            answer[i] = -1
+        else:
+            answer[i] = Min2 + 1
+        Min = i
+    else:
+        answer[i] = Min + 1
+        Min2 = Min
+        Min = i
+
+print(*answer)
