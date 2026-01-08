a52da4464c31b0d0261c439ced7f3904f60f79a7
huizoo <ajtwlsgmlwn@naver.com>
2026-01-08 16:21:52 +0900

    [Silver I] Title: 중앙값 제거, Time: 964 ms, Memory: 355516 KB -BaekjoonHub

--- 백준/Silver/23758. 중앙값 제거/중앙값 제거.py
+++ 백준/Silver/23758. 중앙값 제거/중앙값 제거.py
@@ -8,9 +8,6 @@ cnt = 1
 
 for i in range(0, (N+1)//2):
     # 중앙값 이하의 수들을 2로 계속 나눴을 때 1이 되기까지의 나눗셈 횟수
-    x = arr[i]
-    while x > 1:
-        x >>= 1
-        cnt += 1
+    cnt += arr[i].bit_length() - 1
 
 print(cnt)
