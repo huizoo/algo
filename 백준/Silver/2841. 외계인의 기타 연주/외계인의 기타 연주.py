01878a96e3daf7b087aa7d2c96854cd290604015
huizoo <ajtwlsgmlwn@naver.com>
2026-01-09 18:08:50 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 236 ms, Memory: 111408 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -4,39 +4,17 @@ input = sys.stdin.readline
 N, P = map(int, input().split())
 
 arr = [[] for _ in range(7)]
-arr2 = [0]*7
 cnt = 0
 
 def abc(x, y):
     global cnt
-    now = arr2[x]    
-    if now == 0:
-        arr[x].append(y)
-        arr2[x] = y
+    while arr[x] and arr[x][-1] > y:
+        arr[x].pop()
         cnt += 1
-    elif now > y:
-        while arr[x]:
-            last = arr[x][-1]
-            if last > y:
-                arr[x].pop()
-                cnt += 1
-                arr2[x] = arr[x][-1] if arr[x] else 0
-            elif last < y:
-                arr[x].append(y)
-                arr2[x] = y
-                cnt += 1
-                break
-            else:
-                break
-        else:
-            arr[x].append(y)
-            arr2[x] = y
-            cnt += 1
-    elif now < y:
-        arr[x].append(y)
-        arr2[x] = y
-        cnt += 1       
-    
+    if arr[x] and arr[x][-1] == y:
+        return
+    arr[x].append(y)
+    cnt += 1    
 
 for _ in range(N):
     l, p = map(int, input().split())
