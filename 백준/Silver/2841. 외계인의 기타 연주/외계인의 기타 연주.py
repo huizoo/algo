d16e7d9814ddbc7435ff904a4bce330c10b7ad9a
huizoo <ajtwlsgmlwn@naver.com>
2026-01-09 18:09:49 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 220 ms, Memory: 110968 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -6,18 +6,14 @@ N, P = map(int, input().split())
 arr = [[] for _ in range(7)]
 cnt = 0
 
-def abc(x, y):
-    global cnt
-    while arr[x] and arr[x][-1] > y:
-        arr[x].pop()
-        cnt += 1
-    if arr[x] and arr[x][-1] == y:
-        return
-    arr[x].append(y)
-    cnt += 1    
-
 for _ in range(N):
     l, p = map(int, input().split())
-    abc(l, p)
+    while arr[l] and arr[l][-1] > p:
+        arr[l].pop()
+        cnt += 1
+    if arr[l] and arr[l][-1] == p:
+        continue
+    arr[l].append(p)
+    cnt += 1    
 
 print(cnt)
