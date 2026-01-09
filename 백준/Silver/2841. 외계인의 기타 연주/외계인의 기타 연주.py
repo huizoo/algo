bd345bc099b6028b595aa695106d14caf2da6889
huizoo <ajtwlsgmlwn@naver.com>
2026-01-09 18:00:53 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 276 ms, Memory: 133472 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+N, P = map(int, input().split())
+
+arr = [[] for _ in range(N+1)]
+arr2 = [0]*(N+1)
+cnt = 0
+
+def abc(x, y):
+    global cnt
+    now = arr2[x]    
+    if now == 0:
+        arr[x].append(y)
+        arr2[x] = y
+        cnt += 1
+    elif now > y:
+        while arr[x]:
+            last = arr[x][-1]
+            if last > y:
+                arr[x].pop()
+                cnt += 1
+                arr2[x] = arr[x][-1] if arr[x] else 0
+            elif last < y:
+                arr[x].append(y)
+                arr2[x] = y
+                cnt += 1
+                break
+            else:
+                break
+        else:
+            arr[x].append(y)
+            arr2[x] = y
+            cnt += 1
+    elif now < y:
+        arr[x].append(y)
+        arr2[x] = y
+        cnt += 1       
+    
+
+for _ in range(N):
+    l, p = map(int, input().split())
+    abc(l, p)
+
+print(cnt)
