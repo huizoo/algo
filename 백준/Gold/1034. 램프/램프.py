d197752ea2ebb32b03d12069c2d0e4c797a3777e
huizoo <ajtwlsgmlwn@naver.com>
2026-01-21 13:01:30 +0900

    [Gold IV] Title: 램프, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -3,32 +3,19 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-
 arr = []
 arr2 = [set() for _ in range(M+1)]
 
 for i in range(N):
     row = input().rstrip()
     arr.append(row)
-    cnt = 0
-    for x in row:
-        if x == '0':
-            cnt += 1
-    arr2[cnt].add(row)
+    arr2[row.count('0')].add(row)
 
 K = int(input())
 Max = 0
-if K % 2 == 0:
-    for i in range(min(M-(M%2), K), -1, -2):
+for i in range(M+1):
+    if i <= K and (K - i) % 2 == 0:
         for row in arr2[i]:
-            cnt = arr.count(row)
-            if Max < cnt:
-                Max = cnt
-else:
-    for i in range(min(M-((M+1)%2), K), 0, -2):
-        for row in arr2[i]:
-            cnt = arr.count(row)
-            if Max < cnt:
-                Max = cnt
+            Max = max(Max, arr.count(row))
 
 print(Max)
