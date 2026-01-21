f16e4a91084923cc1b4da7e0367a1b417956395c
huizoo <ajtwlsgmlwn@naver.com>
2026-01-21 12:42:51 +0900

    [Gold IV] Title: 램프, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -1,30 +1,34 @@
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-dic = defaultdict(int)
-arr = [set() for _ in range(51)]
+
+arr = []
+arr2 = [set() for _ in range(M+1)]
 
 for i in range(N):
     row = input().rstrip()
-    dic[row] += 1
+    arr.append(row)
     cnt = 0
     for x in row:
         if x == '0':
             cnt += 1
-    arr[cnt].add(row)
+    arr2[cnt].add(row)
 
 K = int(input())
 Max = 0
 if K % 2 == 0:
-    for i in range(min(50, K), -1, -2):
-        for row in arr[i]:
-            Max = max(dic[row], Max)
+    for i in range(min(M-(M%2), K), -1, -2):
+        for row in arr2[i]:
+            cnt = arr.count(row)
+            if Max < cnt:
+                Max = cnt
 else:
-    for i in range(min(K, 49), 0, -2):
-        for row in arr[i]:
-            Max = max(dic[row], Max)
+    for i in range(min(M-((M+1)%2), K), 0, -2):
+        for row in arr2[i]:
+            cnt = arr.count(row)
+            if Max < cnt:
+                Max = cnt
 
 print(Max)
