2ccef8d88b53d3ebe27cfd3394278802627a9e14
huizoo <ajtwlsgmlwn@naver.com>
2026-01-21 10:45:55 +0900

    [Gold IV] Title: 전구와 스위치, Time: 112 ms, Memory: 34760 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -5,25 +5,25 @@ N = int(input())
 arr = list(map(int, input().rstrip()))
 arr2 = tuple(map(int, input().rstrip()))
 arr3 = arr[:]
-arr3[0] = 1 - arr3[0]
-arr3[1] = 1 - arr3[1]
+arr3[0] ^= 1
+arr3[1] ^= 1
 
 cnt1 = 0
 cnt2 = 1
 for i in range(1, N):
     if arr[i-1] != arr2[i-1]:
         cnt1 += 1
-        arr[i-1] = 1 - arr[i-1]
-        arr[i] = 1 - arr[i]
+        arr[i-1] ^= 1
+        arr[i] ^= 1
         if i+1 < N:
-            arr[i+1] = 1 - arr[i+1]
+            arr[i+1] ^= 1
 
     if arr3[i-1] != arr2[i-1]:
         cnt2 += 1
-        arr3[i-1] = 1 - arr3[i-1]
-        arr3[i] = 1 - arr3[i]
+        arr3[i-1] ^= 1
+        arr3[i] ^= 1
         if i+1 < N:
-            arr3[i+1] = 1 - arr3[i+1]
+            arr3[i+1] ^= 1
 
 if arr[-1] == arr2[-1]:
     if arr3[-1] == arr2[-1]:
