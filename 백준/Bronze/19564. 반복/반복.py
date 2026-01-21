3e4ec260fb8d90b8bdca811dc3f79ab9f3678f1e
huizoo <ajtwlsgmlwn@naver.com>
2026-01-21 22:57:17 +0900

    [Bronze I] Title: 반복, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19564. 반복/반복.py
+++ 백준/Bronze/19564. 반복/반복.py
@@ -2,13 +2,12 @@ import sys
 input = sys.stdin.readline
 S = input().rstrip()
 
-now = ord(S[0])
+now = S[0]
 
 cnt = 0
 for x in S:
-    nxt = ord(x)
-    if now >= nxt:
+    if now >= x:
         cnt += 1
-    now = nxt
+    now = x
 
 print(cnt)
