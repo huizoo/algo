8db4b7a5d0ec290a9fe508cd2e86907b50becc7b
huizoo <ajtwlsgmlwn@naver.com>
2026-01-26 11:25:50 +0900

    [Gold III] Title: 다리 만들기, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,36 +1,53 @@
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
 N = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(N)]
-lands = defaultdict(set)
-visited = [[0]*N for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 d = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-land = 1
+sea = 2
+stack2 = []
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 0: continue
-        if visited[i][j] == 1: continue
-        q = [(i, j)]
-        lands[land].add((i, j))
-        visited[i][j] = 1
-        while q:
-            y, x = q.pop()
-            for dy, dx in d:
-                ny, nx = dy+y, dx+x
-                if ny<0 or nx<0 or ny>=N or nx>=N: continue
-                if arr[ny][nx] == 0: continue
-                if visited[ny][nx] == 1: continue
-                visited[ny][nx] = 1
-                lands[land].add((ny, nx))
-                q.append((ny, nx))
-        land += 1
+        if arr[i][j] == 1:
+            stack = [(i, j)]
+            arr[i][j] = -1
+            while stack:
+                y, x = stack.pop()
+                for dy, dx in d:
+                    ny, nx = dy+y, dx+x
+                    if ny<0 or nx<0 or ny>=N or nx>=N: continue
+                    now = arr[ny][nx]
+                    if now == 1:
+                        arr[ny][nx] = -1
+                        stack.append((ny, nx))
+                    elif now == 0:
+                        arr[ny][nx] = sea
+                        stack2.append((ny, nx))
+                    elif 1 < now < sea:
+                        print(1)
+                        sys.exit()
+                        
+            sea += 1
 
-Min = 1e9
-for i in range(1, land):
-    for y, x in lands[i]:
-        for j in range(i+1, land):
-            for y1, x1 in lands[j]:
-                Min = min(Min, abs(y-y1)+abs(x-x1))
-
-print(Min-1)
+dist = 2
+temp = 0
+while stack2:
+    stack3 = stack2
+    stack2 = []
+    for y, x in stack3:
+        now = arr[y][x]
+        for dy, dx in d:
+            ny, nx = dy+y, dx+x
+            if ny<0 or nx<0 or ny>=N or nx>=N: continue
+            nxt = arr[ny][nx]
+            if nxt == 0:
+                arr[ny][nx] = now
+                stack2.append((ny, nx))
+            elif 1 < nxt < now:
+                temp = dist+1
+            elif now < nxt:
+                print(dist)
+                sys.exit()
+    dist += 2
+    if temp:
+        print(temp)
+        sys.exit()
