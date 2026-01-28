b4d9dfcc9fd00c394ffe545824b5dbafb61f26af
huizoo <ajtwlsgmlwn@naver.com>
2026-01-28 14:59:55 +0900

    [Gold III] Title: 소문난 칠공주, Time: 180 ms, Memory: 112236 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -5,47 +5,67 @@ def main():
     arr = [input().rstrip() for _ in range(5)]
     ans = 0
     d = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    def bhc(y, x):
-        cnt = 1
-        q = [(y, x)]
-        visited2 = [[0]*5 for _ in range(5)]
-        visited2[y][x] = 1
-        while q:
-            yy, xx = q.pop()
-            for dy, dx in d:
-                ny, nx = dy+yy, dx+xx
-                if 0<=ny<5 and 0<=nx<5 and visited[ny][nx] and not visited2[ny][nx]:
-                    visited2[ny][nx] = 1
-                    cnt += 1
-                    q.append((ny, nx))
-        
-        return 1 if cnt == 7 else 0
     
-    def dfs(y, x, Ycnt, Scnt, remain):
+    def update():
+        mask = 0
+        for i in range(5):
+            for j in range(5):
+                if visited[i][j]:
+                    mask |= 1<<(i*5+j)
+        if mask not in seen:
+            seen.add(mask)
+            return 1
+        return 0
+
+    def dfs(Ycnt, Scnt):
         nonlocal ans
-        if Ycnt>=4:
+        if Ycnt == 4:
             return
         if Ycnt+Scnt == 7:
-            if bhc(y, x-1):
-                ans += 1
+            ans += update()
             return
-        if x == 5:
-            y, x = y+1, 0
-        if y == 5:
-            return
-        if Ycnt+Scnt+remain < 7:
-            return
-        visited[y][x] = 1
-        if arr[y][x] == 'S':
-            dfs(y, x+1, Ycnt, Scnt+1, remain-1)
-        else:
-            dfs(y, x+1, Ycnt+1, Scnt, remain-1)  
-        visited[y][x] = 0
+        for y, x in list(cands):
+            lst = []
+            removed = (y, x)
 
-        dfs(y, x+1, Ycnt, Scnt, remain-1)  
+            cands.remove((y, x))
+            visited[y][x] = 1
+
+            for dy, dx in d:
+                ny, nx = dy+y, dx+x
+                if 0<=ny<5 and 0<=nx<5 and not visited[ny][nx]:
+                    if (ny, nx) not in cands:
+                        cands.add((ny, nx))
+                        lst.append((ny, nx))
+        
+            if arr[y][x] == 'S':
+                dfs(Ycnt, Scnt+1)
+            else:
+                dfs(Ycnt+1, Scnt)
+        
+            visited[y][x] = 0
+            for i in lst:
+                cands.remove(i)
+            cands.add(removed)        
 
     visited = [[0]*5 for _ in range(5)]
-    dfs(0, 0, 0, 0, 25)
+    cands = set()
+    seen = set()
+    for i in range(5):
+        for j in range(5):
+            visited[i][j] = 1
+            cands.clear()
+            for di, dj in d:
+                ni, nj = di+i, dj+j
+                if 0<=ni<5 and 0<=nj<5 and not visited[ni][nj]:
+                    cands.add((ni, nj))
+
+            if arr[i][j] == 'S':
+                dfs(0, 1)
+            else:
+                dfs(1, 0)
+            
+
     print(ans)
 
 if __name__ == "__main__":
