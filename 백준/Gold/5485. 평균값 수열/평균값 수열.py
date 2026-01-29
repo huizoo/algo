2be31556f5fa22a83f409bfc5db8ce0c000018d2
huizoo <ajtwlsgmlwn@naver.com>
2026-01-29 10:56:35 +0900

    [Gold II] Title: 평균값 수열, Time: 1168 ms, Memory: 150832 KB -BaekjoonHub

--- 백준/Gold/5485. 평균값 수열/평균값 수열.py
+++ 백준/Gold/5485. 평균값 수열/평균값 수열.py
@@ -1,29 +1,21 @@
 import sys
 input = sys.stdin.readline
+
 N = int(input())
 arr = [int(input()) for _ in range(N)]
 
-start = 2*arr[0]-arr[1]
-cnt = 0
-for i in range(start, arr[0]+1):
-    for j in range(N):
-        if arr[j] < i: break
-        i = 2*arr[j]-i
+INF = 10**12
+lower = -INF
+upper = INF
+
+x = 0
+
+for j in range(N):
+    if j % 2 == 0:
+        upper = min(upper, arr[j]-x)
     else:
-        cnt += 1
+        lower = max(lower, x-arr[j])
 
-print(cnt)
-'''
-2 5 9
-2*2-5 = -1 부터
--1 5 5 13
+    x = 2*arr[j]-x
 
-2*2-4 = 0
-0 4 6 12
-
-2*2-3 = 1
-1 3 7 11
-
-2*2-2 = 2
-2 2 8 10
-'''
+print(max(0, upper-lower+1))
