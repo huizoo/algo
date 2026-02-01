6e1f91e1402e91d983b0ee54a6ce8db04ee1b37b
huizoo <ajtwlsgmlwn@naver.com>
2026-02-02 02:49:28 +0900

    [Silver II] Title: 택배 기사 민서, Time: 148 ms, Memory: 40664 KB -BaekjoonHub

---
+++ 백준/Silver/20954. 택배 기사 민서/택배 기사 민서.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+answer = []
+for _ in range(T):
+    x = int(input())
+    if x == 0:
+        answer.append(x)
+        continue
+    if x > 0:
+        y = 2**(x-1).bit_length()
+        answer.append(y*4-4+x)
+    else:
+        y = 2**(x+1).bit_length()
+        answer.append(y*6-4-x)
+
+print(*answer, sep='\n')
