0337cf0cb49bdd4148fefb9ed5dbddcd9574fe58
huizoo <ajtwlsgmlwn@naver.com>
2026-02-02 17:25:33 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 2312 ms, Memory: 217832 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.py
@@ -7,21 +7,35 @@ for _ in range(M):
     arr[u].append((v, c))
     arr[v].append((u, c))
 
-heap = [(0, 0, A)]
-dist = [1e9]*(N+1)
-dist[A] = 0
-while heap:
-    Max, cost, now = heapq.heappop(heap)
-    if cost > dist[now]:
-        continue
-    if now == B:
-        if cost <= C:
-            print(Max)
-            sys.exit()
-    for nxt, cost1 in arr[now]:
-        cost2 = cost+cost1
-        if dist[nxt] > cost2:
-            dist[nxt] = cost2
-            heapq.heappush(heap, (max(Max, cost1), cost2, nxt))
+def dijkstra(k):
+    heap = [(0, 0, A)]
+    dist = [1e9]*(N+1)
+    dist[A] = 0
+    while heap:
+        cost, Max, now = heapq.heappop(heap)
+        if cost > dist[now]:
+            continue
+        if cost > C:
+            continue
+        if now == B:
+            return 1
+        for nxt, cost1 in arr[now]:
+            Max2 = max(Max, cost1)
+            if Max2 > k:
+                continue
+            cost2 = cost+cost1
+            if dist[nxt] > cost2:
+                dist[nxt] = cost2
+                heapq.heappush(heap, (cost2, Max2, nxt))
 
-print(-1)
+    return 0
+
+l, r = 0, 20
+while l < r:
+    mid = (l+r)//2
+    if dijkstra(mid):
+        r = mid
+    else:
+        l = mid+1
+
+print(l if dijkstra(l) else -1)
