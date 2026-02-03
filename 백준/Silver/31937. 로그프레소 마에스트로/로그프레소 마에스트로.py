b316577dfa85afed7464fd7e8623816ab710f541
huizoo <ajtwlsgmlwn@naver.com>
2026-02-04 03:15:17 +0900

    [Silver II] Title: 로그프레소 마에스트로, Time: 240 ms, Memory: 111872 KB -BaekjoonHub

---
+++ 백준/Silver/31937. 로그프레소 마에스트로/로그프레소 마에스트로.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N, M, K = map(int, input().split())
+virus = set(map(int, input().split()))
+arr = [tuple(map(int, input().split())) for _ in range(M)]
+arr.sort(key=lambda x: x[0])
+
+for v in virus:
+    Set = {v}
+    for t, a, b in arr:
+        if a in Set:
+            Set.add(b)
+    if Set == virus:
+        print(v)
+        sys.exit()
