a75724f3551e9ebaf39554ee7baabaa5a413008a
huizoo <ajtwlsgmlwn@naver.com>
2026-02-07 12:29:34 +0900

    [Gold III] Title: Sleeping in Class, Time: 84 ms, Memory: 43468 KB -BaekjoonHub

--- 백준/Gold/24620. Sleeping in Class/Sleeping in Class.py
+++ 백준/Gold/24620. Sleeping in Class/Sleeping in Class.py
@@ -10,31 +10,28 @@ for _ in range(T):
     arr2[1] = arr[0]
     for i in range(2, N+1):
         arr2[i] = arr2[i-1]+arr[i-1]
-
-
+    Max = max(arr)
+    if Max == 0:
+        answer.append(0)
+        continue
+    Set = set(arr2)
     x = 1
+    total = arr2[-1]
     while True:
         target = arr2[x]
-        if target != 0 and arr2[-1] > target and arr2[-1] // target == 1:
-            answer.append(N-1)
-            break
-        flag = 0
-        cut = 0
-        Sum = 0
-        for i in range(1, N+1):
-            if arr2[i]-arr2[cut] == target:
-                Sum += i-cut-1
-                cut = i
-                flag = 0
-            elif arr2[i]-arr2[cut] < target:
-                flag = 1
-            else:
-                flag = 1
-                break
-        if flag:
+        if target == 0:
             x += 1
+            continue
+        if total % target == 0:
+            cnt = total // target
+            for i in range(2, cnt+1):
+                if i*target not in Set:
+                    x += 1
+                    break
+            else:
+                answer.append(N-cnt)
+                break
         else:
-            answer.append(Sum)
-            break
+            x += 1
 
 print(*answer, sep='\n')
