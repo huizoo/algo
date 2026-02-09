0fa05bbd431746c2fb442e4a51d88d8afe62d8de
huizoo <ajtwlsgmlwn@naver.com>
2026-02-09 14:24:46 +0900

    [Silver V] Title: 투명, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1531. 투명/투명.py
+++ 백준/Silver/1531. 투명/투명.py
@@ -1,13 +1,21 @@
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-arr = [[0]*101 for _ in range(101)]
+arr = [[0]*102 for _ in range(102)]
 for _ in range(N):
     x1, y1, x2, y2 = map(int, input().split())
-    for i in range(y1, y2+1):
-        for j in range(x1, x2+1):
-            arr[i][j] += 1
+    arr[y1][x1] += 1
+    arr[y1][x2+1] -= 1
+    arr[y2+1][x1] -= 1
+    arr[y2+1][x2+1] += 1
     
+for i in range(1, 101):
+    for j in range(1, 101):
+        arr[i][j] += arr[i][j-1]
+for j in range(1, 101):
+    for i in range(1, 101):
+        arr[i][j] += arr[i-1][j]
+
 cnt = 0
 for i in range(1, 101):
     for j in range(1, 101):
