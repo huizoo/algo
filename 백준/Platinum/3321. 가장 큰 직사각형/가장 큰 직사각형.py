966f09559da63fb5e0565f951820421e3134f658
huizoo <ajtwlsgmlwn@naver.com>
2026-02-21 11:08:57 +0900

    [Platinum II] Title: 가장 큰 직사각형, Time: 2056 ms, Memory: 135628 KB -BaekjoonHub

--- 백준/Platinum/3321. 가장 큰 직사각형/가장 큰 직사각형.py
+++ 백준/Platinum/3321. 가장 큰 직사각형/가장 큰 직사각형.py
@@ -2,18 +2,18 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = [list(map(int, input().rstrip())) for _ in range(N)]
-dp = [1 if arr[0][j] == 1 else 0 for j in range(M)]
-S = dp.count(1)
-for i in range(1, N):
-    for j in range(M):
-        if arr[i][j] == 1:
+arr = [input().rstrip() for _ in range(N)]
+S = 0
+dp = [0]*M
+for i in range(N):
+    for j, value in enumerate(arr[i]):
+        if value == '1':
             dp[j] += 1
         else:
             dp[j] = 0
     dp2 = sorted(dp)
-    for k in range(M):
-        S = max(S, dp2[k]*(M-k))
+    for k, value in enumerate(dp2):
+        S = max(S, value*(M-k))
 
 print(S)
     
