f73f435da3802bd2d7429c76db7ef9b4869548dd
huizoo <ajtwlsgmlwn@naver.com>
2026-02-21 09:40:20 +0900

    [Silver I] Title: 시식 코너는 나의 것, Time: 160 ms, Memory: 52408 KB -BaekjoonHub

--- 백준/Silver/23029. 시식 코너는 나의 것/시식 코너는 나의 것.py
+++ 백준/Silver/23029. 시식 코너는 나의 것/시식 코너는 나의 것.py
@@ -1,24 +1,14 @@
 import sys
-sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 N = int(input())
 arr = [int(input()) for _ in range(N)]
-dp = [[-1]*3 for _ in range(N)]
+dp = [[0]*3 for _ in range(N)]
 
-def abc(n, x):
-    if n < 0:
-        return 0
-    
-    if dp[n][x] != -1:
-        return dp[n][x]
+dp[0][1] = arr[0]
 
-    if x == 2:
-        dp[n][x] = abc(n-1, 1) + arr[n]//2
-    elif x == 1:
-        dp[n][x] = abc(n-1, 0) + arr[n]
-    else:
-        dp[n][x] = max(abc(n-1, 0), abc(n-1, 1), abc(n-1, 2))
-    
-    return dp[n][x]
+for i in range(1, N):
+    dp[i][0] = max(dp[i-1])
+    dp[i][1] = dp[i-1][0] + arr[i]
+    dp[i][2] = dp[i-1][1] + arr[i]//2
 
-print(max(abc(N-1, 0), abc(N-1, 1), abc(N-1, 2)))
+print(max(dp[N-1]))
