1b0a44c49fadac9d6de6bdc6a4e65a32f2255d56
huizoo <ajtwlsgmlwn@naver.com>
2026-02-22 08:54:49 +0900

    [Gold IV] Title: 강의실 배정, Time: 416 ms, Memory: 136756 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,23 +1,13 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
 N = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(N)]
+arr = sorted([tuple(map(int, input().split())) for _ in range(N)])
 
-events = []
-
+heap = []
 for s, t in arr:
-    events.append((s, 1))
-    events.append((t, -1))
+    if heap and heap[0] <= s:
+        heapq.heappop(heap)
+    heapq.heappush(heap, t)
 
-events.sort()
-
-cnt = 0
-Max = 0
-
-for time, value in events:
-    cnt += value
-    if cnt > Max:
-        Max = cnt
-
-print(Max)
+print(len(heap))
