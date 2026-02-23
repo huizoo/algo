37104a45fd43152b73d0f8e0ac44cf2c03d0c10d
huizoo <ajtwlsgmlwn@naver.com>
2026-02-23 14:41:49 +0900

    [Silver III] Title: Count Squares, Time: 588 ms, Memory: 267840 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17637. Count Squares/Count Squares.py
+++ 백준/Silver/17637. Count Squares/Count Squares.py
@@ -3,23 +3,17 @@ import sys
 input = sys.stdin.readline
 
 h, v = map(int, input().split())
-Y = list(map(int, input().split()))
-X = list(map(int, input().split()))
+Y = tuple(map(int, input().split()))
+X = tuple(map(int, input().split()))
 
 dic1 = defaultdict(int)
-for i in range(h):
-    for j in range(i+1, h):
-        dic1[Y[j]-Y[i]] += 1
-
-dic2 = defaultdict(int)
-for i in range(v):
-    for j in range(i+1, v):
-        dic2[X[j]-X[i]] += 1
+for idx, value in enumerate(Y):
+    for idx2 in range(idx+1, h):
+        dic1[value-Y[idx2]] += 1
 
 cnt = 0
-for key, value in dic1.items():
-    for key2, value2 in dic2.items():
-        if key == key2:
-            cnt += value*value2
+for idx, value in enumerate(X):
+    for idx2 in range(idx+1, v):
+        cnt += dic1[value-X[idx2]]
 
 print(cnt)
