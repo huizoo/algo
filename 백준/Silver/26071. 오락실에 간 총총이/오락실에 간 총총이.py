c30bc2041bf0c4b2fda5e5cc082da436499e77d1
huizoo <ajtwlsgmlwn@naver.com>
2026-03-01 13:27:27 +0900

    [Silver II] Title: 오락실에 간 총총이, Time: 488 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.py
+++ 백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.py
@@ -1,25 +1,26 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 
-min_x = min_y = N
-max_x = max_y = -1
-
+min_x, min_y = N, N
+max_x, max_y = -1, -1
 cnt = 0
+
 for i in range(N):
-    row = input().rstrip()
-    for j in range(N):
-        if row[j] == 'G':
-            cnt += 1
+    cnt2 = 0
+    for j, v in enumerate(input().rstrip()):
+        if v == 'G':
+            cnt2 += 1
             if min_x > j:
                 min_x = j
             if max_x < j:
                 max_x = j
-            if min_y > i:
-                min_y = i
-            if max_y < i:
-                max_y = i
+    if cnt2:
+        cnt += cnt2
+        if min_y > i:
+            min_y = i
+        if max_y < i:
+            max_y = i
 
 if cnt == 1:
     print(0)
@@ -28,5 +29,10 @@ elif min_x == max_x:
 elif min_y == max_y:
     print(min(N-min_x-1, max_x))
 else:
-    print(min(N-min_x-1, max_x)+min(N-min_y-1, max_y))
+    print(min(N-min_y-1, max_y)+min(N-min_x-1, max_x))
+
+            
+    
+
+
 
