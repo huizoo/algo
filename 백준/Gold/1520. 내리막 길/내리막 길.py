dcb5e57a5d68deb5e425237494476a51e5a62bf6
huizoo <ajtwlsgmlwn@naver.com>
2026-03-02 14:08:06 +0900

    [Gold III] Title: 내리막 길, Time: 276 ms, Memory: 65956 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,26 @@
 import sys
-sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 d = [(1, 0), (0, 1), (0, -1), (-1, 0)]
 M, N = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(M)]
-dp = [[-1]*N for _ in range(M)]
+arr2 = []
+for i, row in enumerate(arr):
+    for j, v in enumerate(row):
+        arr2.append((v, i, j))
 
-def dfs(y, x, now):
-    if y == M-1 and x == N-1:
-        return 1
-    
-    if dp[y][x] != -1:
-        return dp[y][x]
+arr2.sort(reverse=True)
 
-    dp[y][x] = 0
+dp = [[0]*N for _ in range(M)]
+dp[0][0] = 1
+
+for h, y, x in arr2:
+    if dp[y][x] == 0:
+        continue
 
     for dy, dx in d:
         ny, nx = y+dy, x+dx
         if ny<0 or nx<0 or ny>=M or nx>=N: continue
-        if arr[ny][nx] >= now: continue
-        dp[y][x] += dfs(ny, nx, arr[ny][nx])
-    
-    return dp[y][x]
+        if arr[ny][nx] < arr[y][x]:
+            dp[ny][nx] += dp[y][x]
 
-dfs(0, 0, arr[0][0])
-
-
-print(dp[0][0])
+print(dp[M-1][N-1])
