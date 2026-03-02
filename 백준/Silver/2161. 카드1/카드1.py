c79e7acb51089479a9bfd6212a33639ac1643f5a
huizoo <ajtwlsgmlwn@naver.com>
2026-03-02 22:16:58 +0900

    [Silver V] Title: 카드1, Time: 68 ms, Memory: 34936 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,14 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N = int(input())
+ans = [i for i in range(1, N+1, 2)]
+
+q = deque(list(range(2, N+1, 2)))
+if N%2 == 1 and N != 1:
+    q.append(q.popleft())
+while q:
+    ans.append(q.popleft())
+    if q:
+        q.append(q.popleft())
+print(' '.join(map(str, ans)))
