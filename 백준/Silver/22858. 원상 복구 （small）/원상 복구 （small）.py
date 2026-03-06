21d0fa160b2e704542f5eccef62426ea96ef51d1
huizoo <ajtwlsgmlwn@naver.com>
2026-03-06 15:01:51 +0900

    [Silver III] Title: 원상 복구 (small), Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.py
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.py
@@ -5,12 +5,25 @@ N, K = map(int, input().split())
 S = [0] + list(map(int, input().split()))
 D = [0] + list(map(int, input().split()))
 
+visited = [0]*(N+1)
 P = [0]*(N+1)
 
 for i in range(1, N+1):
-    s = i
-    for _ in range(K):
-        s = D[s]
-    P[s] = S[i]
+    if visited[i]:
+        continue
+
+    cycle = []
+    now = i
+
+    while not visited[now]:
+        visited[now] = 1
+        cycle.append(now)
+        now = D[now]
+
+    L = len(cycle)
+    move = K % L
+
+    for j in range(L):
+        P[cycle[j]] = S[cycle[(j-move)%L]]
 
 print(*P[1:])
