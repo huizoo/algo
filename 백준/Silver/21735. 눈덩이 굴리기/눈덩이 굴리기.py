172be136f521ffc6edf98cea032a33b2d49f2f4b
huizoo <ajtwlsgmlwn@naver.com>
2026-03-13 09:29:52 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
@@ -2,11 +2,30 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = [0] + list(map(int, input().split())) + [0, 0]
+arr = [0] + list(map(int,input().split())) + [0, 0]
+
+arr2 = [0]*(N+3)
+for i in range(1, N+3):
+    arr2[i] = arr2[i-1] + arr[i]
+
+Max = 0
 
 def dfs(level, time, size):
-    if time == M or level >= N :
-        return size
-    return max(dfs(level+1, time+1, size+arr[level+1]), dfs(level+2, time+1, size//2+arr[level+2]))
+    global Max
+    if time == M or level >= N:
+        Max = max(Max, size)
+        return
 
-print(dfs(0, 0, 1))
+    x = M - time
+    y = min(N, level + 2*x)
+
+    z = arr2[y] - arr2[level]
+
+    if size + z <= Max:
+        return
+
+    dfs(level+1, time+1, size + arr[level+1])
+    dfs(level+2, time+1, size//2 + arr[level+2])
+
+dfs(0, 0, 1)
+print(Max)
