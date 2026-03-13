37e2c3b1dab9adbee1fbcb4f65a9c82baccfde5c
huizoo <ajtwlsgmlwn@naver.com>
2026-03-13 11:12:33 +0900

    [Gold V] Title: 톱니바퀴, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,85 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-arr = [input().rstrip() for _ in range(4)]
+arr = [input().strip() for _ in range(4)]
+idx = [0]*4
+
 K = int(input())
-idx = [[2, 6] for _ in range(3)]
-
-def right(x, d):
-    l = idx[x][0]
-    r = idx[x][1]
-    if arr[x][l] != arr[x+1][r]:
-        return 1, (l - d) % 8, (r + d) % 8
-    return 0, (l - d) % 8, r
-
-def left(x, d):
-    l = idx[x][0]
-    r = idx[x][1]
-    if arr[x][l] != arr[x+1][r]:
-        return 1, (l + d) % 8, (r - d) % 8
-    return 0, l, (r - d) % 8
 
 for _ in range(K):
     a, b = map(int, input().split())
-    if a == 1:
-        ret = [[-1]*3 for _ in range(3)]
+    a -= 1
 
-        ret[0] = right(0, b)
-        if ret[0][0] == 1:
-            ret[1] = right(1, -b)
-            if ret[1][0] == 1:
-                ret[2] = right(2, b)
+    arr2 = [0]*4
+    arr2[a] = b
 
-        for i in range(3):
-            if ret[i][0] != -1:
-                idx[i][0] = ret[i][1]
-                idx[i][1] = ret[i][2]
+    for i in range(a, 0, -1):
+        r = arr[i][(idx[i]+6)%8]
+        l = arr[i-1][(idx[i-1]+2)%8]
+        if r != l:
+            arr2[i-1] = -arr2[i]
+        else:
+            break
 
-    elif a == 2:
-        ret = [[-1]*3 for _ in range(3)]
+    for i in range(a, 3):
+        r = arr[i][(idx[i]+2)%8]
+        l = arr[i+1][(idx[i+1]+6)%8]
+        if r != l:
+            arr2[i+1] = -arr2[i]
+        else:
+            break
 
-        ret[0] = left(0, b)
-        ret[1] = right(1, b)
-        if ret[1][0] == 1:
-            ret[2] = right(2, -b)
+    for i in range(4):
+        if arr2[i] == 1:
+            idx[i] = (idx[i]-1)%8
+        elif arr2[i] == -1:
+            idx[i] = (idx[i]+1)%8
 
-        for i in range(3):
-            if ret[i][0] != -1:
-                idx[i][0] = ret[i][1]
-                idx[i][1] = ret[i][2]
-        
-    elif a == 3:
-        ret = [[-1]*3 for _ in range(3)]
+ans = 0
+for i in range(4):
+    if arr[i][idx[i]] == '1':
+        ans += 1<<i
 
-        ret[1] = left(1, b)
-        if ret[1][0] == 1:
-            ret[0] = left(0, -b)
-        ret[2] = right(2, b)
-
-        for i in range(3):
-            if ret[i][0] != -1:
-                idx[i][0] = ret[i][1]
-                idx[i][1] = ret[i][2]
-
-    elif a == 4:
-        ret = [[-1]*3 for _ in range(3)]
-
-        ret[2] = left(2, b)
-        if ret[2][0] == 1:
-            ret[1] = left(1, -b)
-            if ret[1][0] == 1:
-                ret[0] = left(0, b)
-
-        for i in range(3):
-            if ret[i][0] != -1:
-                idx[i][0] = ret[i][1]
-                idx[i][1] = ret[i][2]
-
-
-Sum = 0
-Sum += 1 if arr[0][idx[0][0]-2] == '1' else 0
-Sum += 2 if arr[1][idx[0][1]-6] == '1' else 0
-Sum += 4 if arr[2][idx[1][1]-6] == '1' else 0
-Sum += 8 if arr[3][idx[2][1]-6] == '1' else 0
-
-print(Sum)
+print(ans)
