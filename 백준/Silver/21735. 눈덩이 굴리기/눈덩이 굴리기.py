9daf38de936cd59bc1eaa9c77275d0d8b3361a5d
huizoo <ajtwlsgmlwn@naver.com>
2026-03-13 09:32:40 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.py
@@ -1,31 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
+N, M = map(int,input().split())
 arr = [0] + list(map(int,input().split())) + [0, 0]
 
-arr2 = [0]*(N+3)
-for i in range(1, N+3):
-    arr2[i] = arr2[i-1] + arr[i]
+dp = [[-1]*(N+3) for _ in range(M+1)]
+dp[0][0] = 1
 
-Max = 0
+for i in range(M):
+    for j in range(N+1):
+        if dp[i][j] == -1:
+            continue
+        size = dp[i][j]
+        dp[i+1][j+1] = max(dp[i+1][j+1], size + arr[j+1])
+        dp[i+1][j+2] = max(dp[i+1][j+2], size//2 + arr[j+2])
 
-def dfs(level, time, size):
-    global Max
-    if time == M or level >= N:
-        Max = max(Max, size)
-        return
-
-    x = M - time
-    y = min(N, level + 2*x)
-
-    z = arr2[y] - arr2[level]
-
-    if size + z <= Max:
-        return
-
-    dfs(level+1, time+1, size + arr[level+1])
-    dfs(level+2, time+1, size//2 + arr[level+2])
-
-dfs(0, 0, 1)
-print(Max)
+print(max(max(row) for row in dp))
