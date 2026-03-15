438934a1161aff159139147829200547dbeaf944
huizoo <ajtwlsgmlwn@naver.com>
2026-03-15 14:08:47 +0900

    [Gold I] Title: Team Selection, Time: 1104 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/17240. Team Selection/Team Selection.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+dp = [-10**18]*(1<<5)
+dp[0] = 0
+for i in range(N):
+    row = list(map(int, input().split()))
+    for mask in range((1<<5)-1, -1, -1):
+        if dp[mask] < 0:
+            continue
+        for j, x in enumerate(row):
+            if mask & (1<<j):
+                continue
+            
+            mask2 = mask | (1<<j)
+            dp[mask2] = max(dp[mask2], dp[mask] + x)
+
+print(dp[(1<<5)-1])
