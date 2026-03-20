167927671c65919c1b5d7cef5597c2bea06657e5
huizoo <ajtwlsgmlwn@naver.com>
2026-03-20 14:32:33 +0900

    [Silver II] Title: 영일랜드, Time: 172 ms, Memory: 111560 KB -BaekjoonHub

--- 백준/Silver/31871. 영일랜드/영일랜드.py
+++ 백준/Silver/31871. 영일랜드/영일랜드.py
@@ -2,18 +2,17 @@ import sys
 input = sys.stdin.readline
 
 def dfs(now, cost, mask):
-    global Max
-    if now == 0:
-        if mask & check == check:
-            Max = max(Max, cost)
-        return
-    
+    global Max  
     if memo[now][mask] < cost:
         memo[now][mask] = cost
     else:
         return
     
     for nxt, cost2 in arr[now]:
+        if nxt == 0:
+            if mask == check:
+                Max = max(Max, cost+cost2)
+            continue
         if 1<<nxt & mask: continue
         dfs(nxt, cost+cost2, mask | 1<<nxt)
 
@@ -24,9 +23,9 @@ for _ in range(M):
     u, v, w = map(int, input().split())
     arr[u].append((v, w))
 
-check = (1<<N+1)-1
+check = (1<<N+1)-2
 Max = -1
-memo = [[0]*(1<<N+1) for _ in range(N+1)]
+memo = [[-1]*(1<<N+1) for _ in range(N+1)]
 for v, w in arr[0]:
     dfs(v, w, 1<<v)
 
