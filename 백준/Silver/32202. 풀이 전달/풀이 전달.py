686b4f4155c10dce4f7e5207ac0818687dba80a2
huizoo <ajtwlsgmlwn@naver.com>
2026-03-24 15:45:44 +0900

    [Silver I] Title: 풀이 전달, Time: 268 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/32202. 풀이 전달/풀이 전달.py
+++ 백준/Silver/32202. 풀이 전달/풀이 전달.py
@@ -1,12 +1,11 @@
-from array import array
 import sys
 input = sys.stdin.readline
-MOD = 10**9+7
+
+MOD = 10**9 + 7
 N = int(input())
-dp = [array('I', [1]*2) for _ in range(N)]
 
-for i in range(1, N):
-    dp[i][0] = (dp[i-1][0]*2 + dp[i-1][1]) % MOD
-    dp[i][1] = (dp[i-1][0]*2) % MOD
+a, b = 2, 1
+for _ in range(N-1):
+    a, b = (2*(a+b))%MOD, a
 
-print((2*dp[-1][0]+dp[-1][1])%MOD)
+print((a+b)%MOD)
