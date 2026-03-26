e7c4286c5ba499f14861d66f3f04be4305a57a33
huizoo <ajtwlsgmlwn@naver.com>
2026-03-26 13:11:26 +0900

    [Silver III] Title: 계단 오르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -2,11 +2,11 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [[0]*2 for _ in range(N+1)]
-dp[1][0] = int(input())
+arr = [0]+[int(input()) for _ in range(N)]
+dp = [0]*(N+1)
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
 for i in range(2, N+1):
-    x = int(input())
-    dp[i][0] = max(dp[i][0], dp[i-2][0] + x, dp[i-2][1] + x)
-    dp[i][1] = max(dp[i][1], dp[i-1][0] + x)
+    dp[i] = max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i])
 
-print(max(dp[N]))
+print(dp[N])
