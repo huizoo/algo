cb52d33ad6a2310fc60f5e2b48c664dd3900d45d
huizoo <ajtwlsgmlwn@naver.com>
2026-04-04 19:37:33 +0900

    [Gold III] Title: 말아톤, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/2327. 말아톤/말아톤.py
+++ 백준/Gold/2327. 말아톤/말아톤.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 H, N = map(int, input().split())
 arr = sorted([tuple(map(int, input().split())) for _ in range(N)], key=lambda x: x[1], reverse=True)
-dp = [0]*(H+1)
-dp[0] = 1
-for i, (h, s) in enumerate(arr):
-    for hh in range(H, h-1, -1):
-        if dp[hh-h] == 1:
-            dp[hh] = 1
+dp = 1
+mask = (1<<H+1)-1
+check = 1<<H
+for h, s in arr:
+    dp |= (dp<<h)
+    dp &= mask
     
-    if dp[H]:
+    if dp&check:
         print(s)
         break
