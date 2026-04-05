556ebb8cc34aeafc5f409fa13eac6b65de2c845e
huizoo <ajtwlsgmlwn@naver.com>
2026-04-05 20:02:18 +0900

    [Gold I] Title: 동전 뒤집기, Time: 6788 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [0]*N
+for i in range(N):
+    for j, v in enumerate(input().rstrip()):
+        if v == 'T':
+            arr[i] |= 1<<j
+
+Min = N*N
+for mask in range(1<<N):
+    total = 0
+    for x in arr:
+        cnt = (x^mask).bit_count()
+        total += min(cnt, N-cnt)
+    Min = min(Min, total)
+
+print(Min)
