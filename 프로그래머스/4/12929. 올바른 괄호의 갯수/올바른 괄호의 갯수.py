ffff5e2ad5418df496e3460c27be837bbec170c1
huizoo <ajtwlsgmlwn@naver.com>
2026-04-20 18:10:51 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -1,7 +1,4 @@
-def solution(n):
-    N = [0]*15
-    N[0] = 1; N[1] = 1; N[2] = 2; N[3] = 5;
-    for i in range(4, 15):
-        N[i] = sum(N[j]*N[i-1-j] for j in range(i))
-    return N[n]
+from math import comb
 
+def solution(n):
+    return comb(2*n, n) // (n+1)
