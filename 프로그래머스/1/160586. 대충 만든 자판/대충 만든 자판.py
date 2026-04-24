33d9a36892a3c77e19b3d57384e65a6239f7f068
huizoo <ajtwlsgmlwn@naver.com>
2026-04-24 23:46:39 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.42 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,17 +1,17 @@
 def solution(keymap, targets):
     answer = []
-    keymap_dictinary = dict()
-    for i, row in enumerate(keymap):
+    keymap_dictionary = dict()
+    for row in keymap:
         for j, v in enumerate(row):
-            if v in keymap_dictinary:
-                keymap_dictinary[v] = min(j + 1, keymap_dictinary[v])
+            if v in keymap_dictionary:
+                keymap_dictionary[v] = min(j + 1, keymap_dictionary[v])
             else:
-                keymap_dictinary[v] = j + 1
+                keymap_dictionary[v] = j + 1
                 
     for target in targets:
         cnt = 0
         for ch in target:
-            if n:= keymap_dictinary.get(ch, 0):
+            if n:= keymap_dictionary.get(ch, 0):
                 cnt += n
             else:
                 cnt = -1
