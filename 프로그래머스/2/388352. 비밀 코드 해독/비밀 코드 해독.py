de7601120e37144f25c39e3b5878110828e2023e
huizoo <ajtwlsgmlwn@naver.com>
2026-05-01 19:13:49 +0900

    [level 2] Title: 비밀 코드 해독, Time: 178.95 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,26 +1,9 @@
-def solution(n, q, ans):
-    answer = 0
-    combination = 0
-    def dfs(num, cnt):
-        nonlocal answer, combination
-        if cnt == 5:
-            for codes, need in zip(q, ans):
-                match = 0
-                for c in codes:
-                    if combination & (1 << c):
-                        match += 1
-                if match != need:
-                    return
-            answer += 1
-            return
-        if num > n:
-            return
-        if cnt+n-num+1 < 5:
-            return
-        combination |= 1 << num
-        dfs(num+1, cnt+1)
-        combination ^= 1 << num
-        dfs(num+1, cnt)
+import itertools 
 
-    dfs(1, 0)
-    return answer
+def solution(n, q, ans):
+    f = list(itertools.combinations(range(1, n + 1), 5))
+
+    for g, cnt in zip(q, ans):
+         f = [code for code in f if len(set(code) & set(g)) == cnt]
+
+    return len(f)
