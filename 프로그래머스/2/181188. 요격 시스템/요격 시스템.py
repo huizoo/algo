e26ba1b8f88893fb819994483cf6ca0570178798
huizoo <ajtwlsgmlwn@naver.com>
2026-05-01 13:31:38 +0900

    [level 2] Title: 요격 시스템, Time: 428.12 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,12 +1,9 @@
 def solution(targets):
     answer = 0
-    targets.sort()
-    start = end = 0
+    targets.sort(key=lambda x: x[1])
+    end = 0
     for s, e in targets:
         if end <= s:
             answer += 1
-            start = s
-            end = e
-        elif e < end:
             end = e
     return answer
