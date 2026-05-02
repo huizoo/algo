9b09caf8ded11dadfff17e382810cd37470754b9
huizoo <ajtwlsgmlwn@naver.com>
2026-05-02 21:13:40 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 279.54 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,31 +1,15 @@
 def solution(sequence, k):
-    def move_l():
-        nonlocal l, Sum
-        Sum -= sequence[l]
-        l += 1
+    answer = [0, len(sequence) - 1]
+    Sum = 0
+    l = 0
+    for r, v in enumerate(sequence):
+        Sum += v
+                       
+        while Sum > k:
+            Sum -= sequence[l]
+            l += 1
         
-    def move_r():
-        nonlocal r, Sum
-        r += 1
-        Sum += sequence[r]
-    
-    l = r = 0
-    n = len(sequence)
-    sequence.append(0)
-    answer = [0, n-1]
-    Sum = sequence[0]
-    while r < n:
-        if Sum == k:
-            if answer[1] - answer[0] > r - l:
-                answer = [l, r]
-            move_l()        
-            move_r()
-        elif Sum < k:
-            move_r()
-        elif l == r:
-            move_l()
-            move_r()
-        else:
-            move_l()
+        if Sum == k and answer[1] - answer[0] > r - l:
+            answer = [l, r]
         
     return answer
