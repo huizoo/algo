a1ae966e8ffd78fa44f200fd18d4be42367e5987
huizoo <ajtwlsgmlwn@naver.com>
2026-05-04 19:25:54 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 900.84 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,11 +1,15 @@
+from math import isqrt
+
 def get_dot_count(r, pos):
     cnt = 0
     if pos == 'out':
         for x in range(1, r+1):
-            cnt += int((r**2-x**2)**0.5) + 1
+            cnt += isqrt(r**2-x**2) + 1
     elif pos == 'in':
         for x in range(1, r):
-            cnt += int(n) + 1 if (n := (r**2-x**2)**0.5) % 1 != 0 else int(n)
+            y_square = r**2-x**2
+            y = isqrt(y_square)
+            cnt += y if y**2 == y_square else y + 1
         
     return 4 * cnt
 
