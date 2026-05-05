00db9e961fb1ca028c6ba31e8903357a998d97b5
huizoo <ajtwlsgmlwn@naver.com>
2026-05-05 13:10:06 +0900

    [level 2] Title: 광물 캐기, Time: 77.45 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -0,0 +1,40 @@
+def solution(picks, minerals):
+    table = {
+        0: {
+            "diamond": 1,
+            "iron": 1,
+            "stone": 1,
+        },
+        1: {
+            "diamond": 5,
+            "iron": 1,
+            "stone": 1,
+        },
+        2: {
+            "diamond": 25,
+            "iron": 5,
+            "stone": 1,
+        },
+    }
+    
+    answer = 10**9
+    l = len(minerals)
+    def use_pick(mineral_idx, total):
+        nonlocal answer
+        if mineral_idx == l or sum(picks) == 0:
+            if answer > total:
+                answer = total
+            return
+        
+        mj = min(mineral_idx + 5, l)
+        for i in range(3):
+            if picks[i] > 0:
+                picks[i] -= 1
+                fatigue = 0
+                for j in range(mineral_idx, mj):
+                    fatigue += table[i][minerals[j]]
+                use_pick(mj, total+fatigue)
+                picks[i] += 1
+    
+    use_pick(0, 0)
+    return answer
