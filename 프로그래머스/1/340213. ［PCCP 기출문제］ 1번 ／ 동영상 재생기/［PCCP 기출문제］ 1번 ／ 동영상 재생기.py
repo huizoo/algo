d4e68141ac670f9fc1bfe8cda3efc35ee34f4603
huizoo <ajtwlsgmlwn@naver.com>
2026-05-06 12:10:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.05 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,33 +1,30 @@
-def parse_time(time):
-    arr = time.split(':')
-    return int(arr[0])*60 + int(arr[1])
-
 def solution(video_len, pos, op_start, op_end, commands):
-    video_time = parse_time(video_len)
-    pos_time = parse_time(pos)
-    op_start_time = parse_time(op_start)
-    op_end_time = parse_time(op_end)
+    def get_second(time):
+        return 60*int(time[0:2])+int(time[3:5])
+    
+    
+    def is_op(time):
+        if op_start <= time <= op_end:
+            return True
+        return False
+    
+    video_len = get_second(video_len)
+    pos = get_second(pos)
+    op_start = get_second(op_start)
+    op_end = get_second(op_end)   
+    
+    if is_op(pos):
+        pos = op_end
     
     for command in commands:
-        if op_start_time <= pos_time < op_end_time:
-            pos_time = op_end_time
-        if command == 'prev':
-            pos_time -= 10
-            if op_start_time <= pos_time < op_end_time:
-                pos_time = op_end_time
-            elif pos_time < 0:
-                pos_time = 0
+        if command == "next":
+            pos = min(pos+10, video_len)
+            if is_op(pos):
+                pos = op_end                
         else:
-            pos_time += 10
-            if op_start_time <= pos_time < op_end_time:
-                pos_time = op_end_time
-            elif pos_time > video_time:
-                pos_time = video_time
-    
-    minute = pos_time // 60
-    second = pos_time % 60
-    if second < 10:
-        second = '0' + str(second)
-    if minute < 10:
-        minute = '0' + str(minute)
-    return f'{minute}:{second}'
+            pos = max(pos-10, 0)
+            if is_op(pos):
+                pos = op_end
+    minute = pos//60
+    second = pos%60
+    return ':'.join((f"0{minute}" if minute < 10 else f"{minute}", f"0{second}" if second < 10 else f"{second}"))
