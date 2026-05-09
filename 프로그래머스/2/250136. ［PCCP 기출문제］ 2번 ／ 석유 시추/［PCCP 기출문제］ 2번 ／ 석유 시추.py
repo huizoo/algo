3648a7d7a9cde6f6bca056e526cdddf8ebf01fc8
huizoo <ajtwlsgmlwn@naver.com>
2026-05-09 21:22:37 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 150.12 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,31 +1,37 @@
-from collections import deque
-
-d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
 def solution(land):
     n, m = len(land), len(land[0])
     visited = [[0]*m for _ in range(n)]
-    Sum = [0]*m
-    for j in range(m):
-        for i in range(n):
-            if land[i][j] == 0: continue
-            if visited[i][j] == 1: continue
-            q = deque()
-            q.append((i, j))
-            visited[i][j] = 1
-            cnt = 1
-            minx, maxx = j, j
-            while q:
-                nowy, nowx = q.popleft()
-                for dy, dx in d:
-                    ny, nx = nowy + dy, nowx + dx
-                    if ny < 0 or nx < 0 or ny >= n or nx >= m: continue
-                    if land[ny][nx] == 0: continue
-                    if visited[ny][nx] == 1: continue
-                    visited[ny][nx] = 1
-                    q.append((ny, nx))
-                    cnt += 1
-                    minx, maxx = min(minx, nx), max(maxx, nx)
-            for k in range(minx, maxx+1):
-                Sum[k] += cnt
-    return max(Sum)
+    d = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    prefix = [0]*(m+1)
+    for i, row in enumerate(land):
+        for j, v in enumerate(row):
+            if v == 1 and not visited[i][j]:
+                l = r = j
+                stack = []
+                stack.append((i, j))
+                cnt = 1
+                visited[i][j] = 1
+                while stack:
+                    y, x = stack.pop()
+                    if x < l:
+                        l = x
+                    elif r < x:
+                        r = x
+                    for dy, dx in d:
+                        ny, nx = dy+y, dx+x
+                        if ny<0 or nx<0 or ny>=n or nx>=m: continue
+                        if not land[ny][nx]: continue
+                        if visited[ny][nx]: continue
+                        stack.append((ny, nx))
+                        visited[ny][nx] = 1
+                        cnt += 1
+                prefix[l] += cnt
+                prefix[r+1] -= cnt
+    Max = 0
+    Sum = 0
+    for x in prefix:
+        Sum += x
+        if Max < Sum:
+            Max = Sum
+    
+    return Max
