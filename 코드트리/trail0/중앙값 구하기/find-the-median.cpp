a92f5ffcac0961e79bb591865ab3a913bff4a648
huizoo <ajtwlsgmlwn@naver.com>
2026-05-21 04:28:57 +0900

    [Passed] 중앙값 구하기 (2ms, 0MB)

---
+++ 코드트리/trail0/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b) {
+        if (b > c) {
+            cout << b;
+        } else if (c > a) {
+            cout << a;
+        } else {
+            cout << c;
+        }
+    } else {
+        if (a > c) {
+            cout << a;
+        } else if (b > c) {
+            cout << c;
+        } else {
+            cout << b;
+        }
+    }
+    return 0;
+}
