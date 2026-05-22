7dda587acef109151c5651b7ed443cafff56de36
huizoo <ajtwlsgmlwn@naver.com>
2026-05-22 16:07:35 +0900

    [Passed] 1부터의 합 (2ms, 0MB)

---
+++ 코드트리/trail0/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+    for (int i=1; i<=100; i++) {
+        cnt += i;
+        if (cnt >= n) {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
