30fa02011764231c50e3b5c5da10ac50ebccc659
huizoo <ajtwlsgmlwn@naver.com>
2026-05-22 16:23:05 +0900

    [Passed] 배수의 개수 (1ms, 0MB)

---
+++ 코드트리/trail0/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, cnt3=0, cnt5=0;
+    for (int i=0; i<10; i++) {
+        cin >> a;
+        if (a%3 == 0) {
+            cnt3 += 1;
+        }
+        if (a%5 == 0) {
+            cnt5 += 1;
+        }
+    }
+    cout << cnt3 << ' ' << cnt5;
+    return 0;
+}
