62f2cb911405f2127d831527c07818ff4df8327d
huizoo <ajtwlsgmlwn@naver.com>
2026-05-26 22:24:16 +0900

    [Passed] 데이터 교환 (2ms, 0MB)

---
+++ 코드트리/trail1/데이터 교환/exchange-data.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6, c = 7, temp;
+    temp = a;
+    a = c;
+    c = b;
+    b = temp;
+    cout << a << '\n' << b << '\n' << c;
+    return 0;
+}
