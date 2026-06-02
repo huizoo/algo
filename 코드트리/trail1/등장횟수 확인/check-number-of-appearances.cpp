bd97f57e9efdec54ac9c10cad8974760c2257dff
huizoo <ajtwlsgmlwn@naver.com>
2026-06-03 02:54:06 +0900

    [Passed] 등장횟수 확인 (2ms, 0MB)

---
+++ 코드트리/trail1/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt=0;
+    int n;
+    for (int i=0; i<5; i++) {
+        cin >> n;
+        if (n%2==0) cnt += 1;
+    }
+    cout << cnt;
+    return 0;
+}
