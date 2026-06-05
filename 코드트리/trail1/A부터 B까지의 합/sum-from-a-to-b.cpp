69a99a81f0422f3c982e35a4dad321d8098cc759
huizoo <ajtwlsgmlwn@naver.com>
2026-06-05 14:18:55 +0900

    [Passed] A부터 B까지의 합 (1ms, 0MB)

---
+++ 코드트리/trail1/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int total = 0;
+    for (int i=a; i<=b; i++) {
+        total += i;
+    }
+    cout << total;
+    return 0;
+}
