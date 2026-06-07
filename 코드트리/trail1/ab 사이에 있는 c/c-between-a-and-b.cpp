2869e1d7a3828205d04ef1043889aa43534a7db5
huizoo <ajtwlsgmlwn@naver.com>
2026-06-08 02:48:31 +0900

    [Passed] ab 사이에 있는 c (2ms, 0MB)

---
+++ 코드트리/trail1/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    bool found = false;
+    for (int i=a; i<=b; i++) {
+        if (i%c==0) {
+            found = true;
+            break;
+        }
+    }
+    if (found) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
