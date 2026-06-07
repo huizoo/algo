af1766cbd090e000625834c4a48422a3ae5da987
huizoo <ajtwlsgmlwn@naver.com>
2026-06-08 02:28:35 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ 코드트리/trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while (true) {
+        cin >> n;
+        if (n != 0) {
+            cout << n << '\n';
+        } else break;
+    }
+    return 0;
+}
