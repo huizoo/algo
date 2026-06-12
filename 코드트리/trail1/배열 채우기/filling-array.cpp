5d5f8791e84cd2cae41524383d7cf87aa29a868c
huizoo <ajtwlsgmlwn@naver.com>
2026-06-13 00:14:30 +0900

    [Passed] 배열 채우기 (2ms, 0MB)

---
+++ 코드트리/trail1/배열 채우기/filling-array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr(10);
+    int n = 10;
+    for (int i=0; i<10; i++) {
+        cin >> arr[i];
+        if (arr[i] == 0) {n=i; break;}
+    }
+    for (int i=n-1; i>=0; i--) {
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
