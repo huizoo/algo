0adbb8509171228a26dbdf6a6ae775a0f5b347f8
huizoo <ajtwlsgmlwn@naver.com>
2026-07-27 08:58:35 +0900

    [level 2] Title: 귤 고르기, Time: 13.44 ms, Memory: 23 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -2,18 +2,8 @@ from collections import Counter
 
 def solution(k, tangerine):
     answer = 0
-    table = Counter(tangerine)
-    cnt = 0
-    for t, c in sorted(table.items(), key=lambda x: -x[1]):
-        if cnt + c > k:
-            answer += 1
-            cnt = k
+    for idx, cnt in enumerate(sorted(Counter(tangerine).values(), reverse=True), start=1):
+        if (k:=k-cnt) <= 0:
+            answer = idx
             break
-        elif cnt + c == k:
-            answer += 1
-            cnt += c
-            break
-        else:
-            answer += 1
-            cnt += c
     return answer
