7f74744180b8c8384a39b13ab7ccf0a4f4c1d256
huizoo <ajtwlsgmlwn@naver.com>
2026-07-28 09:39:20 +0900

    [level 2] Title: 택배상자, Time: 112.44 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,22 +1,18 @@
 def solution(order):
     answer = 0
-    sub_last = 0
+    sub = [0]
     truck = 1
-    used = [0]*(len(order)+1)
+    n = len(order)
+    
     for o in order:
-        if truck == o:
-            used[truck] = 1
+        while truck <= n and truck <= o:
+            sub.append(truck)
             truck += 1
-        elif sub_last == o:
-            used[sub_last] = 1
-            while used[sub_last]:
-                sub_last -= 1
-        elif sub_last+1 < o:
-            sub_last = o-1
-            used[o] = 1
-            truck = o+1
+        
+        if sub[-1] == o:
+            sub.pop()
+            answer += 1
         else:
             break
-            
-    answer = sum(used)
+    
     return answer
