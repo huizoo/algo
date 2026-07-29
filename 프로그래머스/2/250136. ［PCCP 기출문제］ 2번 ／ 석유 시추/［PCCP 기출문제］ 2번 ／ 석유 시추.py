8895a01e53386d26d0699b7d3ece95a1ce9cd8e5
huizoo <ajtwlsgmlwn@naver.com>
2026-07-29 09:29:15 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 198.44 ms, Memory: 23.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,16 +1,16 @@
 def solution(land):
     n, m = len(land), len(land[0])
+    d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     visited = [[0]*m for _ in range(n)]
-    d = [(0, 1), (1, 0), (-1, 0), (0, -1)]
     prefix = [0]*(m+1)
     for i, row in enumerate(land):
         for j, v in enumerate(row):
-            if v == 1 and not visited[i][j]:
+            if v == 1 and visited[i][j] == 0:
                 l = r = j
                 stack = []
                 stack.append((i, j))
-                cnt = 1
                 visited[i][j] = 1
+                cnt = 1
                 while stack:
                     y, x = stack.pop()
                     if x < l:
@@ -19,19 +19,16 @@ def solution(land):
                         r = x
                     for dy, dx in d:
                         ny, nx = dy+y, dx+x
-                        if ny<0 or nx<0 or ny>=n or nx>=m: continue
-                        if not land[ny][nx]: continue
-                        if visited[ny][nx]: continue
-                        stack.append((ny, nx))
-                        visited[ny][nx] = 1
-                        cnt += 1
+                        if 0<=ny<n and 0<=nx<m:
+                            if land[ny][nx] == 0: continue
+                            if visited[ny][nx] == 1: continue
+                            stack.append((ny, nx))
+                            visited[ny][nx] = 1
+                            cnt += 1
                 prefix[l] += cnt
                 prefix[r+1] -= cnt
-    Max = 0
-    Sum = 0
-    for x in prefix:
-        Sum += x
-        if Max < Sum:
-            Max = Sum
     
-    return Max
+    for i in range(m):
+        prefix[i+1] += prefix[i]
+    
+    return max(prefix)
