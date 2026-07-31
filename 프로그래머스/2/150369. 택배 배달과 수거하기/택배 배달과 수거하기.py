43658004ef203a2cc921ee6fd241d2f30b687fba
huizoo <ajtwlsgmlwn@naver.com>
2026-07-31 09:47:19 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 821.54 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -3,45 +3,59 @@ def solution(cap, n, deliveries, pickups):
     di = pi = n - 1
     dlist = []
     plist = []
-    
+
     while di >= 0:
+        while di >= 0 and deliveries[di] == 0:
+            di -= 1
+
+        if di < 0:
+            break
+
         remain = cap
         far = di
         remove = 0
+
         while di >= 0 and remain - deliveries[di] >= 0:
             remain -= deliveries[di]
             remove += deliveries[di]
             di -= 1
-        else:
-            if remain > 0 and deliveries[di]:        
-                deliveries[di] -= remain
-                remove += remain
-        
+
+        if di >= 0 and remain > 0:
+            deliveries[di] -= remain
+            remove += remain
+
         if remove > 0:
             dlist.append(far)
-        
+
     while pi >= 0:
+        while pi >= 0 and pickups[pi] == 0:
+            pi -= 1
+
+        if pi < 0:
+            break
+
         remain = cap
         far = pi
         remove = 0
+
         while pi >= 0 and remain - pickups[pi] >= 0:
             remain -= pickups[pi]
             remove += pickups[pi]
             pi -= 1
-        else:
-            if remain > 0 and pickups[pi]:        
-                pickups[pi] -= remain
-                remove += remain
-                
+
+        if pi >= 0 and remain > 0:
+            pickups[pi] -= remain
+            remove += remain
+
         if remove > 0:
-            plist.append(far)    
-        
+            plist.append(far)
+
     if len(dlist) < len(plist):
-        dlist += [0]*(len(plist)-len(dlist))
+        dlist += [0] * (len(plist) - len(dlist))
     else:
-        plist += [0]*(len(dlist)-len(plist))
-        
+        plist += [0] * (len(dlist) - len(plist))
+
     for d, p in zip(dlist, plist):
         answer += max(d, p) + 1
-        
-    return 2*answer
+
+    return 2 * answer
