d6b3718b8f1cc2ece57735da1a01d4622bd48988
huizoo <ajtwlsgmlwn@naver.com>
2026-07-31 09:37:25 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 640.41 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,47 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    di = pi = n - 1
+    dlist = []
+    plist = []
+    
+    while di >= 0:
+        remain = cap
+        far = di
+        remove = 0
+        while di >= 0 and remain - deliveries[di] >= 0:
+            remain -= deliveries[di]
+            remove += deliveries[di]
+            di -= 1
+        else:
+            if remain > 0 and deliveries[di]:        
+                deliveries[di] -= remain
+                remove += remain
+        
+        if remove > 0:
+            dlist.append(far)
+        
+    while pi >= 0:
+        remain = cap
+        far = pi
+        remove = 0
+        while pi >= 0 and remain - pickups[pi] >= 0:
+            remain -= pickups[pi]
+            remove += pickups[pi]
+            pi -= 1
+        else:
+            if remain > 0 and pickups[pi]:        
+                pickups[pi] -= remain
+                remove += remain
+                
+        if remove > 0:
+            plist.append(far)    
+        
+    if len(dlist) < len(plist):
+        dlist += [0]*(len(plist)-len(dlist))
+    else:
+        plist += [0]*(len(dlist)-len(plist))
+        
+    for d, p in zip(dlist, plist):
+        answer += max(d, p) + 1
+        
+    return 2*answer
