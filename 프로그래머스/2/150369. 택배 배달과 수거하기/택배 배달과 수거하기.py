764abec10d0eae7230ad25cd993613e99b0fde67
huizoo <ajtwlsgmlwn@naver.com>
2026-07-31 09:53:27 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 23.62 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,61 +1,20 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    di = pi = n - 1
-    dlist = []
-    plist = []
+    delivery = 0
+    pickup = 0
 
-    while di >= 0:
-        while di >= 0 and deliveries[di] == 0:
-            di -= 1
+    for i in range(n - 1, -1, -1):
+        delivery += deliveries[i]
+        pickup += pickups[i]
 
-        if di < 0:
-            break
+        delivery_count = (delivery + cap - 1) // cap
+        pickup_count = (pickup + cap - 1) // cap
 
-        remain = cap
-        far = di
-        remove = 0
+        count = max(0, delivery_count, pickup_count)
 
-        while di >= 0 and remain - deliveries[di] >= 0:
-            remain -= deliveries[di]
-            remove += deliveries[di]
-            di -= 1
+        answer += 2 * (i + 1) * count
 
-        if di >= 0 and remain > 0:
-            deliveries[di] -= remain
-            remove += remain
+        delivery -= count * cap
+        pickup -= count * cap
 
-        if remove > 0:
-            dlist.append(far)
-
-    while pi >= 0:
-        while pi >= 0 and pickups[pi] == 0:
-            pi -= 1
-
-        if pi < 0:
-            break
-
-        remain = cap
-        far = pi
-        remove = 0
-
-        while pi >= 0 and remain - pickups[pi] >= 0:
-            remain -= pickups[pi]
-            remove += pickups[pi]
-            pi -= 1
-
-        if pi >= 0 and remain > 0:
-            pickups[pi] -= remain
-            remove += remain
-
-        if remove > 0:
-            plist.append(far)
-
-    if len(dlist) < len(plist):
-        dlist += [0] * (len(plist) - len(dlist))
-    else:
-        plist += [0] * (len(dlist) - len(plist))
-
-    for d, p in zip(dlist, plist):
-        answer += max(d, p) + 1
-
-    return 2 * answer
+    return answer
