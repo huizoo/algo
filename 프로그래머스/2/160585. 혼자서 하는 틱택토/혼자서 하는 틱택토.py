25de1a249cb3aaecb7306ca5b33d788fe6c181e1
huizoo <ajtwlsgmlwn@naver.com>
2026-09-01 12:17:35 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.02 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,29 +1,43 @@
-def solution(board):
-    board2 = list(''.join(row) for row in zip(*board))
-    o_cnt = sum(row.count('O') for row in board)
-    x_cnt = sum(row.count('X') for row in board)
-    o_win = (
-        'OOO' in board or
-        'OOO' in board2 or
-        board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O' or
-        board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O'
-    )
-    x_win = (
-        'XXX' in board or
-        'XXX' in board2 or
-        board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X' or
-        board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X'
-    )
+def solution(board):    
+    o_cnt = sum(1 if v == 'O' else 0 for row in board for v in row)
+    x_cnt = sum(1 if v == 'X' else 0 for row in board for v in row)
     
-    if x_cnt > o_cnt:
-        return 0
-    if o_cnt > x_cnt + 1:
-        return 0
-    if o_win and x_win:
-        return 0
-    if o_win and o_cnt != x_cnt + 1:
-        return 0
-    if x_win and o_cnt != x_cnt:
+    '''
+    O 빙고 상태라면?
+    o_cnt == x_cnt + 1
+    '''              
+    for row in board:
+        if row.count('O') == 3 and o_cnt != x_cnt + 1:
+            return 0
+    
+    for row in zip(*board):
+        if row.count('O') == 3 and o_cnt != x_cnt + 1:
+            return 0
+    
+    if (board[0][0] == board[1][1] == board[2][2] == 'O' or
+        board[2][0] == board[1][1] == board[0][2] == 'O') and o_cnt != x_cnt + 1:
+            return 0
+    
+    '''
+    X 빙고 상태라면?
+    o_cnt == x_cnt
+    '''
+    for row in board:
+        if row.count('X') == 3 and o_cnt != x_cnt:
+            return 0
+    
+    for row in zip(*board):
+        if row.count('X') == 3 and o_cnt != x_cnt:
+            return 0
+        
+    if (board[0][0] == board[1][1] == board[2][2] == 'X' or
+        board[2][0] == board[1][1] == board[0][2] == 'X') and o_cnt != x_cnt:
+            return 0
+    '''
+    빙고가 없다면?
+    o_cnt == x_cnt or o_cnt == x_cnt + 1
+    '''
+    if not (o_cnt == x_cnt or o_cnt == x_cnt + 1):
         return 0
     
     return 1
